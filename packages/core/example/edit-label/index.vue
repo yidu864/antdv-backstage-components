78dd7765fd2e734ca5cd030ba20d1e34e23f3e73
yidu864 <943070101@qq.com>
2022-01-06 15:08:22 +0800

    feat(core): init core-example

---
+++ packages/core/example/edit-label/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="edit-label-example">edit-label-example</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+/**
+ * example for core-component edit-label
+ */
+@Component({ name: 'edit-label-example' })
+export default class EditLabelExample extends Vue {
+  created() {}
+  mounted() {}
+}
+</script>
+
+<style lang="scss" scoped>
+.edit-label-example {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- packages/core/example/index.vue
+++ packages/core/example/index.vue
@@ -2,16 +2,41 @@
   <!--
     CORE-TEST
   -->
-  <div class="core-test">
-    <div class="core-test-wrap">
-      <edit-label v-model="value"></edit-label>
-    </div>
-  </div>
+  <a-layout class="core-example">
+    <a-layout-header class="core-example-header">
+      <div class="logo">component</div>
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :default-selected-keys="['2']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1"> core </a-menu-item>
+        <a-menu-item key="2"> wrap </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content class="core-example-wrap">
+      <div class="left-section">
+        <div
+          v-for="item in compList"
+          :key="item.value"
+          :class="['section-item', { 'section-item__active': item.value === checkComp }]"
+          @click="checkComp = item.value"
+        >
+          {{ item.label }}
+        </div>
+      </div>
+      <div class="right-content">
+        <component v-if="comp" :is="comp" />
+        <span v-else>暂无组件</span>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import EditLabel from '../components/edit-label/index.vue'
+import compMap from './config'
 
 /**
  * core 库测试环境
@@ -20,21 +45,83 @@ import EditLabel from '../components/edit-label/index.vue'
  * @input none
  * @output none
  */
-@Component({ name: 'core-test', components: { EditLabel } })
+@Component({ name: 'core-test' })
 export default class CoreTest extends Vue {
-  value = 'test'
+  // checked = 'test'
+
+  get compList() {
+    return Object.keys(compMap).map(v => ({ label: v, value: v }))
+  }
+
+  checkComp = this.compList[0].value
+
+  get comp() {
+    return compMap[this.checkComp]
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.core-test {
-  width: 100%;
+.core-example {
   height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  .logo {
+    width: 120px;
+    height: 31px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px 24px 16px 0;
+    float: left;
+    color: #fff;
+    line-height: 31px;
+    text-align: center;
+  }
+  &-header {
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+  }
   &-wrap {
-    width: 300px;
+    padding: 74px 10px 10px 0;
+    height: 100%;
+    display: flex;
+    .left-section {
+      width: 250px;
+      height: 100%;
+      background-color: #fff;
+      border-right: 1px solid #e8e8e8;
+    }
+    .right-content {
+      flex: 1;
+      box-sizing: border-box;
+      padding: 10px;
+    }
+    .section-item {
+      height: 40px;
+      line-height: 40px;
+      font-size: 14px;
+      padding: 0 16px 0 40px;
+      cursor: pointer;
+      position: relative;
+      &::before {
+        content: '';
+        display: block;
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 3px;
+        transition: height 0.3s linear;
+        height: 0%;
+        background-color: #1890ff;
+        visibility: hidden;
+      }
+      &__active {
+        background-color: #e6f7ff;
+        &:before {
+          height: 100%;
+          visibility: visible;
+        }
+      }
+    }
   }
 }
 </style>
