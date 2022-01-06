ceb4de3cc58610b4e20cb20f32070e6a9e813708
yidu864 <943070101@qq.com>
2022-01-06 15:06:39 +0800

    chore(config file):  update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,14 @@ module.exports = {
   },
   // extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
   extends: [
+    // 'plugin:vue/essential',
+    // 'eslint:recommended',
+    // '@vue/typescript/recommended',
+    // '@vue/prettier',
+    // '@vue/prettier/@typescript-eslint'
     'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    // '@vue/standard',
+    '@vue/typescript'
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -17,7 +20,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'no-case-declarations': 'off'
   },
   overrides: [
     {
